8a6dd797fb9fe594d7f4f74593be222bea30036c
Juan Carlos Armengod <juanka85am@gmail.com>
2020-07-25 17:12:38 +0200

    cadenas

--- 18_cadenas.js
+++ 18_cadenas.js
@@ -4,3 +4,12 @@ const cadenaSinEspacios = cadena.split(' ').join('').toUpperCase()
 const cadenaInversa = cadena.split(' ').reverse().join('').toUpperCase()
 console.log(cadenaSinEspacios)
 console.log(cadenaInversa)
+
+const aDatos = [12, 3098, 400, 60, 7]
+const aDatosOriginal = aDatos.slice(0) // funcion inmutable
+aDatos.sort( (a, b) => a - b) // Funcion mutable
+console.log(aDatos)
+console.log(aDatosOriginal)
+
+const dobles = aDatos.map(item => item * 2)
+console.log(dobles)
